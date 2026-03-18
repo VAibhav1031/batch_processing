0af7487d8123c5d02bef79e4090d20d63cc63100
Vaibhav Tiwari <vaibhavgrand2004@gmail.com>
2026-03-18 12:26:34 +0530

    feat: updated the max_tries and recovery option

--- batching/manager.go
+++ batching/manager.go
@@ -2,6 +2,7 @@ package batching
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"io"
 	"os"
@@ -9,6 +10,7 @@ import (
 	// "encoding/json"
 
 	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/mailru/easyjson"
 
@@ -130,20 +132,19 @@ func BatchCollector(jobChan chan Task, finalBatchChan chan []Task, redisBatchCha
 	}
 }
 
+func isConnectionError(err error) bool {
+	var connEr *pgconn.ConnectError
+	if errors.As(err, &connEr) {
+		return true
+	}
+	return false
+}
+
 // there is a two  thing we have to work on  first is that  we have to open the file and  put all the task thing  batch-wise , and after sucessfull commit we remove or clear the file , i would say so , and there would  condition like even before putting thing in the batch file , wer have to check it hasa to tbe empty , it is just like aground , wehere we can have thing  if it hads content alrteady there then we habve
 
 // securrity-wise this  file need to be important ,  like for  anyone taking over our server (which rarely -but can ) is that they can target the thing to these file where they can really make , privilege or  some rule  we have to decide
 // there should be one function (go routine) which check is there
 
-func saveToFile() {
-	file, err := os.OpenFile("batch_segment.txt", os.O_WRONLY|os.O_APPEND, 0)
-	if err != nil {
-		log.Fatal("Failed to open the file")
-	}
-
-	defer file.Close()
-
-}
 func (w *Worker) StartDBWorker(id int, finalBatchChan chan []Task) {
 
 	file_name := fmt.Sprintf("buffer_worker_%d.tmp", id)
@@ -162,44 +163,87 @@ func (w *Worker) StartDBWorker(id int, finalBatchChan chan []Task) {
 	// pos,err := file.Seek(0,io.SeekCurrent)  // here 0 means no change in the cursor position , whence is the relative position , sicne cursor is not moved and whence is SeekCurrent would give current position of the cursor and we will use that  to determine thing
 
 	tries := 0
+
 	info, err := file.Stat()
 	if err == nil && info.Size() > 0 {
-		log.Println("Recovering uncommited data ")
+		log.Println("Recovering UNCOMMITED DATA ")
 		//  wer have to commit to the db ,  plus we have to maintain the per worker max retries if we say so , using map or something
 
 		// read from the file and then
 		data, err := io.ReadAll(file)
-		if err != nil {
+		if err == nil {
 			var recoveredBatch TaskList
 
 			if err := easyjson.Unmarshal(data, &recoveredBatch); err == nil {
-				w.dbCommit(context.Background(), recoveredBatch, file)
+				err = w.dbCommit(context.Background(), recoveredBatch, file)
+
+				if err != nil {
+					log.Printf("Worker %d: Recovery commit failed")
+
+					file.Close()
+					deadletter := fmt.Sprintf("dead_letter_RECOVERY_%d_%d.json", id, time.Now().Unix())
+					os.Rename(file_name, deadletter)
+
+					file, _ = os.OpenFile(file_name, os.O_RDWR|os.O_CREATE, 0666)
+
+				} else {
+					log.Printf("Worker %d: RECOVERY Sucessful.", id)
+				}
+			} else {
+				log.Printf("Worker %d: Recovery file Corrupted deleting..", id)
+				file.Truncate(0) //Unable to Marshal , file get corrupted means unusual data or something
 			}
 		}
 
 		file.Seek(0, 0)
 	}
 
+	// the above thing is for the crash scenario , like just in between the commit  system get crashed then  we  have one check which handle that
 	for batch := range finalBatchChan {
-		log.Println("RECEIEVED!! the BATCH") // check here the file is empty or not ,and then if not we call the function crash replay
+		log.Printf("Worker %d: RECEIEVED!! the BATCH", id) // check here the file is empty or not ,and then if not we call the function crash replay
 
-		// saving to the file
-
-		jsonData, err := easyjson.Marshal(TaskList(batch))
+		jsonData, err := easyjson.Marshal(TaskList(batch)) // write the log/data to the file
 		if err == nil {
+			file.Truncate(0)
+			file.Seek(0, 0)
 			file.Write(jsonData)
 		}
 		err = w.dbCommit(context.Background(), batch, file) //let this function be the private it is not needed to be imported
 
 		if err != nil {
-			tries++
+			// entering the loop of
+			for {
 
-			if tries >= 3 {
+				log.Printf("Worker %d: Trying to commit Again ", id)
+				err = w.dbCommit(context.Background(), batch, file)
 
-				log.Printf("Worker %d: Poison Pill detected , Moving  to dead letter", id)
-				file.Close() // we must close the file , before renaming thing , cause
-				os.Rename(file_name, fmt.Sprintf("dead_letter_%d_%d.json", id, time.Now().Unix()))
+				if err == nil {
+					// successfull
+					tries = 0
+					break
+				}
+				if isConnectionError(err) {
+					log.Println("Database is down. Waiting 5 seconds...")
+					time.Sleep(5 * time.Second)
+					continue // Try the EXACT SAME batch again
+				}
+				tries++
+				if tries >= 3 {
+					log.Printf("Worker %d: Poison Pill detected , Moving  to dead letter", id)
+					file.Close() // we must close the file , before renaming thing , cause there is no use of this now , fd must be close and that is this fd is of no use and in future similar requirement we would open a new file for that request
+					os.Rename(file_name, fmt.Sprintf("dead_letter_%d_%d.json", id, time.Now().Unix()))
+
+					file, open_error := os.OpenFile(file_name, os.O_RDWR|os.O_CREATE, 0666)
+					if open_error != nil {
+						log.Printf("Worker %d: Failed to REOPEN-Buffer %d", id, file)
+					}
+
+					tries = 0 // after the dead-end we have to update the tries
+					break
+
+				}
 			}
+
 		}
 
 	}
@@ -222,7 +266,7 @@ func (w *Worker) dbCommit(ctx context.Context, batch []Task, file *os.File) erro
 
 	if err != nil {
 
-		log.Printf("Batch commit failed:", err)
+		log.Printf("Batch commit failed: %v", err)
 		return err
 	}
 	log.Printf("Batch Commited Successfully")
